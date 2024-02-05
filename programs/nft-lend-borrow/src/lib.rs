8986d3b5174bf032bcafbef5ab0d4da2081d9cdf
vanhauhoang <158867636+vanhauhoang@users.noreply.github.com>
2024-02-05 10:10:39 +0700

    Update id

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("FRQpianJhSMz3vuY5p9JfRmPenxZdtMh3c53YP6KvkrS");
 
 #[program]
 pub mod nft_lend_borrow {
